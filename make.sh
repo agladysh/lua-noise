de61c010ee94c32bc85b77d36cab139233c4679e
Alexander Gladysh <agladysh@gmail.com>
2009-08-22 01:51:02 +0400

    make.sh: tuned build flags

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gcc src/perlin.c $LDFLAGS -llua -dynamiclib -undefined dynamic_lookup -o perlin.so
+gcc src/perlin.c -Werror -Wall --pedantic -c89 $LDFLAGS -llua -dynamiclib -undefined dynamic_lookup -o perlin.so
