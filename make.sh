baf0172745bc6ae83ca8a593634e983d19925d85
Alexander Gladysh <agladysh@gmail.com>
2009-08-22 11:34:56 +0400

    make.sh: noted that it works under OS X only

--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
 
+# Note: This builds under OS X.
+# Under Linux you'd need other flags to create .so.
+
 gcc src/perlin.c -O2 -Werror -Wall --pedantic -c89 $LDFLAGS -llua -dynamiclib -undefined dynamic_lookup -o perlin.so
