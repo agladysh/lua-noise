9a02411dc56455c8f40d25dfc88bd8f857b7e8ce
Alexander Gladysh <agladysh@gmail.com>
2009-08-22 11:19:34 +0400

    make.sh: now building with -O2

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-gcc src/perlin.c -Werror -Wall --pedantic -c89 $LDFLAGS -llua -dynamiclib -undefined dynamic_lookup -o perlin.so
+gcc src/perlin.c -O2 -Werror -Wall --pedantic -c89 $LDFLAGS -llua -dynamiclib -undefined dynamic_lookup -o perlin.so
